33ed0b345d319d8e0a8c0d3a0fe5ea3c72aabbf1
VKarpina <v.karpina01@gmail.com>
2024-12-11 14:20:41 +0100

    added 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import FriendList from "./components/FriendList/FriendList";
 import Profile from "./components/Profile/Profile";
+import userData from "./assets/userData.json";
+import friends from "./assets/friends.json";
 
 const App = () => {
   return (
     <>
-      <Profile />
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
     </>
   );
 };

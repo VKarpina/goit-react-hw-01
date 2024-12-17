5b278e9d6d0374cc5918ac2827f172dfafa8a5bc
VKarpina <v.karpina01@gmail.com>
2024-12-17 13:31:31 +0100

    final

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,34 @@
+import clsx from "clsx";
+import styles from "./Profile.module.css";
+
 const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div>
+    <div className={styles.card}>
       <div>
-        <img src={image} alt="User avatar" />
-        <p>{name}</p>
-        <p>@{tag}</p>
-        <p>{location}</p>
+        <img
+          className={styles.image}
+          src={image}
+          alt="User avatar"
+          width="100"
+          height="100"
+        />
+        <p className={styles.title}>{name}</p>
+        <p className={styles.tag}>@{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
 
-      <ul>
-        <li>
+      <ul className={styles.list}>
+        <li className={clsx(styles.item, styles.radiusFirst)}>
           <span>Followers</span>
-          <span>{stats.followers}</span>
+          <span className={styles.bold}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={clsx(styles.item, styles.border)}>
           <span>Views</span>
-          <span>{stats.views}</span>
+          <span className={styles.bold}>{stats.views}</span>
         </li>
-        <li>
+        <li className={clsx(styles.item, styles.radiusSecond)}>
           <span>Likes</span>
-          <span>{stats.likes}</span>
+          <span className={styles.bold}>{stats.likes}</span>
         </li>
       </ul>
     </div>
